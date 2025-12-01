d4de45390a5f4ff8fc2b3cc1d6f300660b8632fd
rdaniz <dani_pmz@hotmail.com>
2025-12-01 14:35:14 +0100

    Modelo funcionando

--- tareas_dwes/tareas/admin.py
+++ tareas_dwes/tareas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+# Esto lo importa Django automáticamente, pero no funciona, en cambio del otro modo si
+# from tareas_dwes.tareas.models import Tarea
+
+from .models import Tarea
+
 # Register your models here.
+admin.site.register(Tarea)
